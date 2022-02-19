87593bb153bbfa3ca8b17204b33afa6c69d300cb
jhin93 <scene1993@gmail.com>
2022-02-19 15:22:00 +0900

    컨테이너 1 웹 완료

--- client/src/components/Concept.js
+++ client/src/components/Concept.js
@@ -17,6 +17,7 @@ const ConceptStyle = styled.div`
     }
 
     .container1_intro {
+        padding: 6vw 0;
         width: 80%;
         height: 100%;
         display: flex;
@@ -24,12 +25,21 @@ const ConceptStyle = styled.div`
         justify-content: space-between;
         .container1_intro_text {
             color: white;
-            .headline {
-                font-family: 'Anton', sans-serif;
+            /* background-color: darkgray; */
+            /* margin: -20px 0px; */
+            .c1_headline {
                 font-size: 4.8vw;
+                font-family: 'Anton', sans-serif;
+            }
+            .c1_body {
+                /* font-size: 1.2vw; */
             }
         }
-        padding: 6vw 0;
+
+        .intro_image {
+            /* background-color: darkgrey; */
+            padding: 18px 0px;
+        }
     }
 
 `
@@ -44,16 +54,16 @@ function Concept () {
                 <div className="concept_container1">
                     <div className="container1_intro">
                         <div className="container1_intro_text">
-                            <div className="headline">WELCOME TO THE<br/> PIRATESDAO</div>
-                            <p>
-                            PiratesDAO는 클레이튼 체인 위에 살고 있는<br /> 
-                            10000개의 대체 불가능한 토큰입니다.<br />
-                            각각의 고양이들은 200여개의 독특한 파트들로 구성되어<br />
-                            세상에 오직 하나만 존재하는 특별한 수집품입니다.<br />
-                            <br />
-                            또한 www.piratesdao.org에서 프로필로 사용할 수 있으며<br />
-                            앞으로 추가될 선술집(커뮤니티), 해도(캘린더), 현상금수배서(시즌제 랭킹 시스템)<br />
-                            소셜네트워크, 디파이, 게임들의 기능들을 사용할 수 있습니다.
+                            <div className="c1_headline">WELCOME TO THE<br/> PIRATESDAO</div>
+                            <p className="c1_body">
+                                PiratesDAO는 클레이튼 체인 위에 살고 있는<br /> 
+                                10000개의 대체 불가능한 토큰입니다.<br />
+                                각각의 고양이들은 200여개의 독특한 파트들로 구성되어<br />
+                                세상에 오직 하나만 존재하는 특별한 수집품입니다.<br />
+                                <br />
+                                또한 www.piratesdao.org에서 프로필로 사용할 수 있으며<br />
+                                앞으로 추가될 선술집(커뮤니티), 해도(캘린더), 현상금수배서(시즌제 랭킹 시스템)<br />
+                                소셜네트워크, 디파이, 게임들의 기능들을 사용할 수 있습니다.
                             </p>
                         </div>
                         <div>
