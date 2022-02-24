ff4d26b4df49945da6e8ee9bf9ece00716d8e597
jhin93 <scene1993@gmail.com>
2022-02-24 17:22:23 +0900

    일부 페이지 헤더폰트 링크수정

--- client/src/components/Crew.js
+++ client/src/components/Crew.js
@@ -321,6 +321,7 @@ export default function Crew() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Dokdo&family=East+Sea+Dokdo&family=Gothic+A1&family=Gowun+Batang&family=Nanum+Brush+Script&family=Roboto:wght@900&family=Rock+Salt&family=Walter+Turncoat&display=swap" rel="stylesheet" />
       <div className="container">
         <div className="mainContents">
             <Test></Test>
--- client/src/components/Vault/Vault.js
+++ client/src/components/Vault/Vault.js
@@ -112,7 +112,7 @@ export default function Vault() {
             <VaultStyle >
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Dokdo&family=East+Sea+Dokdo&family=Gothic+A1&family=Gowun+Batang&family=Nanum+Brush+Script&family=Roboto:wght@900&family=Rock+Salt&family=Walter+Turncoat&display=swap" rel="stylesheet" />
                 <div className="container">
                       <div className="mainContent">
                         <TemporaryDrawer />
